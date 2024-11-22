76b99f4edaa75d163d16c5e14506bd1221c63ae3
Shabbir Rajput <sabbirrajput03@gmail.com>
2024-11-22 19:11:57 +0530

    Added Path Url Strategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router_demo/ui/route/route.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
+  setPathUrlStrategy();
   runApp(const MyApp());
 }
 
